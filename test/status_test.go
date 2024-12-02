42ff2c2283509ed19fd508beee81d9c763c8f038
shutstart <chirag.cool200@gmail.com>
2024-12-02 10:20:51 +0100

    initial commit for health-check

---
+++ test/status_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"testing"
+
+	pb "github.com/clyso/ceph-api/api/gen/grpc/go"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func Test_List_Ceph_Status(t *testing.T) {
+	r := require.New(t)
+	client := pb.NewStatusClient(admConn)
+	res, err := client.GetCephStatus(tstCtx, &emptypb.Empty{})
+	r.NoError(err)
+	r.NotEmpty(res)
+	r.NotEmpty(res.Fsid)
+	r.NotEmpty(res.QuorumAge)
+	// TODO: Add more checks here
+}
