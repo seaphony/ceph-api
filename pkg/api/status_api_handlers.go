5afa79a73bac8d426dae092a0a03dda46d4bfe44
shutstart <chirag.cool200@gmail.com>
2024-12-09 17:46:49 +0100

    Squashed commit for mon dump feature

--- pkg/api/status_api_handlers.go
+++ pkg/api/status_api_handlers.go
@@ -6,8 +6,10 @@ import (
 
 	pb "github.com/clyso/ceph-api/api/gen/grpc/go"
 	"github.com/clyso/ceph-api/pkg/rados"
+	"github.com/clyso/ceph-api/pkg/types"
 	"github.com/clyso/ceph-api/pkg/user"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func NewStatusAPI(radosSvc *rados.Svc) pb.StatusServer {
@@ -36,4 +38,41 @@ func (s *statusAPI) GetCephStatus(ctx context.Context, body *emptypb.Empty) (*pb
 	}
 
 	return &statusDump, nil
+}
+
+func (s *statusAPI) GetCephMonDump(ctx context.Context, req *emptypb.Empty) (*pb.CephMonDumpResponse, error) {
+	if err := user.HasPermissions(ctx, user.ScopeMonitor, user.PermRead); err != nil {
+		return nil, err
+	}
+
+	const cmdTempl = `{"prefix": "mon dump", "format": "json"}`
+	res, err := s.radosSvc.ExecMon(ctx, cmdTempl)
+	if err != nil {
+		return nil, err
+	}
+	var monDump types.CephMonDumpResponse
+	if err := json.Unmarshal(res, &monDump); err != nil {
+		return nil, err
+	}
+
+	modifiedTimestamp := timestamppb.New(*monDump.Modified)
+	createdTimestamp := timestamppb.New(*monDump.Created)
+	response := pb.CephMonDumpResponse{
+		Epoch:             monDump.Epoch,
+		Fsid:              monDump.Fsid,
+		Modified:          modifiedTimestamp,
+		Created:           createdTimestamp,
+		MinMonRelease:     monDump.MinMonRelease,
+		MinMonReleaseName: monDump.MinMonReleaseName,
+		ElectionStrategy:  monDump.ElectionStrategy,
+		DisallowedLeaders: monDump.DisallowedLeaders,
+		StretchMode:       monDump.StretchMode,
+		TiebreakerMon:     monDump.TiebreakerMon,
+		RemovedRanks:      monDump.RemovedRanks,
+		Features:          monDump.Features,
+		Mons:              monDump.Mons,
+		Quorum:            monDump.Quorum,
+	}
+
+	return &response, nil
 }
